78693f9fa0697661fcd26108be5fafca93c1a0a9
Divakar <dp00498064@techmahindra.com>
2018-02-01 19:42:30 +0530

    Added Assest to iOS Folder. Entry Custom renderer. Login screen XAML page

---
+++ NWG/NWG/Custom Renderer/MyEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NWG
+{
+    public class MyEntry : Entry
+    {
+        
+    }
+}
---
+++ NWG/iOS/Custom Renderer/BorderLessEntryRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS; using Xamarin.Forms; using System.ComponentModel;
+using NWG.iOS.CustomRenderer;
+using NWG;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(BorderLessEntryRenderer))]
+namespace NWG.iOS.CustomRenderer
+{
+    public class BorderLessEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)         {             base.OnElementPropertyChanged(sender, e);              Control.Layer.BorderWidth = 0;              Control.BorderStyle = UIKit.UITextBorderStyle.None;         }
+    }
+}
