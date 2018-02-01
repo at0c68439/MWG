47e56988ece1696a4dc8c40f80e8734f8a835cfc
pratik mittal <prateekmittal1994@gmail.com>
2018-02-01 21:45:50 +0530

    ExcavationInfo page

---
+++ NWG/NWG/ExcavationInfoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NWG
+{
+    public partial class ExcavationInfoPage : ContentPage
+    {
+        public ExcavationInfoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
