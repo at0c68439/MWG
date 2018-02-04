3158493498430dc062abfd2a25cc87ffcf57aa96
asheesh <c30327@bswhealth.org>
2018-02-05 00:29:15 +0530

    login validation

--- NWG/NWG/View/Login.xaml.cs
+++ NWG/NWG/View/Login.xaml.cs
@@ -16,21 +16,43 @@ namespace NWG.View
         }
         protected void btn_loginclicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new Dashboard());
 
             var accountList = new UserManager().getAllUser();
-            var account = accountList.Find((it) => it.UserName.Equals(username.Text.Trim().ToString()) && it.Password.Equals(password.Text.Trim().ToString()));
 
-            if (account != null)
+            if (string.IsNullOrEmpty(username.Text) && string.IsNullOrEmpty(password.Text))
             {
-                Settings.UserName = account.UserName;
-                Settings.Password = account.Password;
-                Settings.Role = account.Role;
+                DisplayAlert("Required", "Please Enter Username And Password", "OK");
             }
-            else {
-                
-                DisplayAlert("Authentication Failed", "Please Enter Correct Password","OK");
+            else if (string.IsNullOrEmpty(username.Text))
+            {
+
+                DisplayAlert("Required", "Please Enter Username", "OK");
+
             }
+            else if (string.IsNullOrEmpty(password.Text))
+            {
+
+                DisplayAlert("Required", "Please Enter Password", "OK");
+            }
+            else
+            {
+                var account = accountList.Find((it) => it.UserName.Equals(username.Text.Trim().ToString()) && it.Password.Equals(password.Text.Trim().ToString()));
+
+                if (account != null)
+                {
+                    Settings.UserName = account.UserName;
+                    Settings.Password = account.Password;
+                    Settings.Role = account.Role;
+
+                    Navigation.PushAsync(new Dashboard());
+                }
+                else
+                {
+
+                    DisplayAlert("Authentication Failed", "Please Enter Correct Password", "OK");
+                }
+            }
+           
         }
     }
 }
